0eca8bfb14072d11bae408994edfc3b50a7444ae
Joseph Knowles <joejknowles@gmail.com>
2016-08-14 13:04:32 +0200

    tests added for counter reducer

--- app.js
+++ app.js
@@ -6,4 +6,17 @@ expect(
   counter(0, { type: 'INCREMENT'})
 ).toEqual(1);
 
-document.write('hi')
+expect(
+  counter(1, { type: 'INCREMENT'})
+).toEqual(2);
+
+expect(
+  counter(2, { type: 'DECREMENT'})
+).toEqual(1);
+
+expect(
+  counter(1, { type: 'DECREMENT'})
+).toEqual(0);
+
+
+document.write('tests passed')
